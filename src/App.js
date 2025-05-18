b59dbfd630fb35b76bc817618e485653cf54df11
ooooo333i <antmdrl0531@gmail.com>
2025-05-18 17:50:10 +0900

    feature/UI/adding card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
+import Card from './components/card';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>리스트</h1>
+      <Card variant="outlined">{Card}</Card>
 
     </div>
   );
