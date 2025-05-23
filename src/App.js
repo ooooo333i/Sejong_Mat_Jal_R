826b41fff9c266128f5b0589c7b95e0750432015
박민수 <qkralstn8332@sju.ac.kr>
2025-05-23 15:14:04 +0900

    feature/UI/update App.js for selectedTags Array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ScrollView from './components/scrollview';
+import TagSelector from './components/Tag';
 
 function App() {
+  const [selectedTags, setSelectedTags] = useState([]);
+
   return (
     <div className="App" style={{ height: '100vh' }}>
-      <ScrollView />
+      <TagSelector selectedTags={selectedTags} onTagChange={setSelectedTags} />
+      <ScrollView selectedTags={selectedTags} />
     </div>
   );
 }
