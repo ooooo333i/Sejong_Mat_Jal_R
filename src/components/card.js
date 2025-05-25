98b740310ce4570278b7814474447740a1ec92e7
ooooo333i <antmdrl0531@gmail.com>
2025-05-25 21:08:11 +0900

    hotfix

--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,16 @@
-import * as React from "react";
+import React from "react";
+import {
+  Card, CardHeader, CardMedia, CardContent, CardActions, Collapse,
+  Avatar, IconButton, Typography
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import { blue } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import {
+  Favorite as FavoriteIcon,
+  Share as ShareIcon,
+  ExpandMore as ExpandMoreIcon,
+  MoreVert as MoreVertIcon
+} from "@mui/icons-material";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
